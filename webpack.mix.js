a955dae526b9137a6a50f51c3a30e51e39a9d7d2
StereoFlo <StereoFlo@users.noreply.github.com>
2017-10-19 22:11:47 +0300

    moved for cms

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,4 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.js('engine/resources/assets/js/app.js', 'public/js').sass('engine/resources/assets/sass/app.scss', 'public/css');
