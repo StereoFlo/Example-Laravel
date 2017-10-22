ce44552242b6652ea984d836fe1061987e337663
StereoFlo <StereoFlo@users.noreply.github.com>
2017-10-22 12:48:12 +0300

    ajax test

--- engine/app/Http/Controllers/Auth/LoginController.php
+++ engine/app/Http/Controllers/Auth/LoginController.php
@@ -2,6 +2,8 @@
 
 namespace RecycleArt\Http\Controllers\Auth;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Request;
 use RecycleArt\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
@@ -23,5 +25,25 @@ class LoginController extends Controller
     {
         $this->redirectTo = config('user.redirectAuth');
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * The user has been authenticated.
+     *
+     * @param \Illuminate\Http\Request|Request $request
+     * @param  mixed                           $user
+     *
+     * @return mixed
+     */
+    protected function authenticated(Request $request, $user)
+    {
+        if (!$request->ajax()){
+            return $this;
+        }
+        return response()->json([
+            'auth' => Auth::check(),
+            'user' => $user,
+            'intended' => $this->redirectPath(),
+        ]);
     }
 }
