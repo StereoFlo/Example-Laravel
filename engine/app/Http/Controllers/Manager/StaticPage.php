971c1b95177f97b8ae5d1b500d8b916d442ce983
StereoFlo <StereoFlo@users.noreply.github.com>
2017-10-23 15:54:38 +0300

    StaticPage controller

---
+++ engine/app/Http/Controllers/Manager/StaticPage.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace RecycleArt\Http\Controllers\Manager;
+
+use Illuminate\Http\Request;
+use RecycleArt\Http\Controllers\Controller;
+
+class StaticPage extends Controller
+{
+    //
+}
