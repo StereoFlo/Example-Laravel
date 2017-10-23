4f3880cf8df1f37b5ce8b30109386e1440526c28
StereoFlo <StereoFlo@users.noreply.github.com>
2017-10-23 12:08:50 +0300

    ajax login

--- engine/app/Http/Controllers/Auth/LoginController.php
+++ engine/app/Http/Controllers/Auth/LoginController.php
@@ -4,8 +4,9 @@ namespace RecycleArt\Http\Controllers\Auth;
 
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Request as RequestLogin;
+use Illuminate\Http\Request;
 use RecycleArt\Http\Controllers\Controller;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
@@ -32,24 +33,164 @@ class LoginController extends Controller
      */
     public function ajaxLogin()
     {
-        return view('auth.login-ajax');
+        return view('auth.ajax.login');
     }
 
     /**
-     * @param RequestLogin $request
-     * @param              $user
+     * Show the application's login form.
      *
-     * @return $this|\Illuminate\Http\JsonResponse
+     * @return \Illuminate\Http\Response
      */
-    protected function authenticated(RequestLogin $request, $user)
+    public function showLoginForm()
     {
-        if (!$request->ajax()){
-            return $this;
+        return view('auth.login');
+    }
+
+    /**
+     * Handle a login request to the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return mixed
+     */
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+        if ($this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
         }
-        return response()->json([
-            'auth' => Auth::check(),
-            'user' => $user,
-            'intended' => $this->redirectPath(),
+
+        if ($this->attemptLogin($request)) {
+            return $this->sendLoginResponse($request);
+        }
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
+    }
+
+    /**
+     * Validate the user login request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     */
+    protected function validateLogin(Request $request)
+    {
+        $this->validate($request, [
+            $this->username() => 'required|string',
+            'password' => 'required|string',
         ]);
+    }
+
+    /**
+     * Attempt to log the user into the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    protected function attemptLogin(Request $request)
+    {
+        return $this->guard()->attempt(
+            $this->credentials($request), $request->filled('remember')
+        );
+    }
+
+    /**
+     * Get the needed authorization credentials from the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    protected function credentials(Request $request)
+    {
+        return $request->only($this->username(), 'password');
+    }
+
+    /**
+     * Send the response after the user was authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    protected function sendLoginResponse(Request $request)
+    {
+        $request->session()->regenerate();
+
+        $this->clearLoginAttempts($request);
+
+        return $this->authenticated($request, $this->guard()->user())
+            ?: redirect()->intended($this->redirectPath());
+    }
+
+    /**
+     * The user has been authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $user
+     * @return mixed
+     */
+    protected function authenticated(Request $request, $user)
+    {
+        if ($request->ajax()) {
+            return response()->json([
+                'auth' => Auth::check(),
+                'user' => $user
+            ]);
+        }
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     * @throws ValidationException
+     */
+    protected function sendFailedLoginResponse(Request $request)
+    {
+        if ($request->ajax()) {
+            return response()->json([
+                'auth' => Auth::check(),
+                'user' => null
+            ]);
+        }
+        throw ValidationException::withMessages([
+            $this->username() => [trans('auth.failed')],
+        ]);
+    }
+
+    /**
+     * Get the login username to be used by the controller.
+     *
+     * @return string
+     */
+    public function username()
+    {
+        return 'email';
+    }
+
+    /**
+     * Log the user out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+
+        $request->session()->invalidate();
+
+        return redirect('/');
+    }
+
+    /**
+     * Get the guard to be used during authentication.
+     *
+     * @return \Illuminate\Contracts\Auth\StatefulGuard
+     */
+    protected function guard()
+    {
+        return Auth::guard();
     }
 }
---
+++ engine/resources/views/auth/ajax/login.blade.php
@@ -0,0 +1,33 @@
+<form id="ajaxLogin" class="signIn" method="POST" action="{{ route('login') }}" onsubmit="return false;">
+    {{ csrf_field() }}
+
+    <div class="inputGroup{{ $errors->has('email') ? ' has-error' : '' }}">
+        <label for="email">email:</label>
+        <input id="email" class="signIn__email" type="email" name="email" value="{{ old('email') }}" required autofocus>
+        <i class="fa fa-user" aria-hidden="true"></i>
+        <span class="errorText">
+            @if ($errors->has('email'))
+                <strong>{{ $errors->first('email') }}</strong>
+            @endif
+        </span>
+    </div>
+    <div class="inputGroup{{ $errors->has('password') ? ' has-error' : '' }}">
+        <label for="password">пароль:</label>
+        <input id="password" class="signIn__pass" type="password" name="password" required>
+        <i class="fa fa-unlock-alt" aria-hidden="true"></i>
+        <span class="errorText">
+            @if ($errors->has('password'))
+                <strong>{{ $errors->first('password') }}</strong>
+            @endif
+        </span>
+    </div>
+    <div class="inputGroup">
+        <label for="remember">Запомнить e-mail ?</label><input type="checkbox" id="remember" name="remember" {{old('remember') ? 'checked' : ''}}/>
+    </div>
+    <div class="signIn__buttons">
+        <a href="#" class="signIn__forgot">забыл пароль :(</a>
+        <button id="ajaxLoginButton" type="button" name="button" class="button signIn__enter">войти</button>
+    </div>
+    <a href="#" class="signIn__toRegisterBtn">Регистрация</a>
+
+</form>
