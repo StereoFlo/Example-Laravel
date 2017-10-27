ae2b9f77df690f94f67054d839b8fb88b352892a
Evgeniy <e.bronnikov@petrovich.ru>
2017-10-27 11:47:37 +0300

    a little fixes

--- engine/app/Models/User.php
+++ engine/app/Models/User.php
@@ -14,9 +14,9 @@ class User extends Authenticatable
     /**
      * test
      */
-    const ROLE_ADMIN = 'admin';
+    const ROLE_ADMIN     = 'admin';
     const ROLE_MODERATOR = 'moderator';
-    const ROLE_AUTHOR = 'author';
+    const ROLE_AUTHOR    = 'author';
 
     /**
      * The attributes that are mass assignable.
@@ -24,7 +24,13 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'location', 'phone', 'about'
+        'name',
+        'email',
+        'password',
+        'location',
+        'phone',
+        'about',
+        'avatar',
     ];
 
     /**
@@ -33,12 +39,143 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
     ];
 
-    public static function getInstance()
+    /**
+     * @var \Illuminate\Contracts\Auth\Authenticatable|null
+     */
+    protected $currentUser;
+
+    /**
+     * @return self
+     */
+    public static function getInstance(): self
     {
         return new self();
+    }
+
+    /**
+     * @return \Illuminate\Contracts\Auth\Authenticatable|null
+     */
+    public function getCurrentUser()
+    {
+        return $this->currentUser;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return self
+     */
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     *
+     * @return self
+     */
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLocation(): string
+    {
+        return $this->location;
+    }
+
+    /**
+     * @param string $location
+     *
+     * @return self
+     */
+    public function setLocation(string $location): self
+    {
+        $this->location = $location;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPhone(): string
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @param string $phone
+     *
+     * @return self
+     */
+    public function setPhone(string $phone): self
+    {
+        $this->phone = $phone;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAbout(): string
+    {
+        return $this->about;
+    }
+
+    /**
+     * @param string $about
+     *
+     * @return self
+     */
+    public function setAbout(string $about): self
+    {
+        $this->about = $about;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAvatar(): string
+    {
+        return $this->avatar;
+    }
+
+    /**
+     * @param string $avatar
+     *
+     * @return self
+     */
+    public function setAvatar(string $avatar): self
+    {
+        $this->avatar = $avatar;
+        return $this;
     }
 
     /**
