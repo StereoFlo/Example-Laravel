2c51e4eb2cd14dcb6772ff24174c646c43653922
Evgeniy <e.bronnikov@petrovich.ru>
2017-11-02 08:55:31 +0300

    fixes

--- engine/resources/views/errors/401.blade.php
+++ engine/resources/views/errors/401.blade.php
@@ -4,7 +4,7 @@
     <section class="error">
         <img src="{{ url('static/images/recycle_err.png') }}" alt="" class="error__pic">
         <p class="error__code">401</p>
-        <p class="error__text">Требуется авторизация! <br> Приносим свои извинения.</p>
+        <p class="error__text">У вас нет прав на просмотр этого раздела</p>
         <p class="error__desc"></p>
         <a href="{{ url('index.html') }}" class="error__link">Главная страница</a>
     </section>
