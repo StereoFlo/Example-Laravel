1640fb8cbd0ac5e880b007061de7e07f5e9784f5
StereoFlo <StereoFlo@users.noreply.github.com>
2017-11-05 21:19:44 +0300

    https

---
+++ engine/app/Http/Middleware/ForceHttps.php
@@ -0,0 +1,28 @@
+<?php
+namespace RecycleArt\Http\Middleware;
+
+use Closure;
+
+use Illuminate\Http\Request;
+
+class ForceHttps
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!app()->environment('local')) {
+            Request::setTrustedProxies([$request->getClientIp()]);
+            if (!$request->isSecure()) {
+                return redirect()->secure($request->getRequestUri());
+            }
+        }
+
+        return $next($request);
+    }
+}
