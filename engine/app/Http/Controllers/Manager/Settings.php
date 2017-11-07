bcd8a095dee731b702d948ba9a482362222cdb04
Evgeniy <e.bronnikov@petrovich.ru>
2017-11-07 09:55:04 +0300

    manager fix

---
+++ engine/app/Http/Controllers/Manager/Settings.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace RecycleArt\Http\Controllers\Manager;
+
+use Illuminate\Http\Request;
+use RecycleArt\Http\Controllers\Controller;
+
+/**
+ * Class Settings
+ * @package RecycleArt\Http\Controllers\Manager
+ */
+class Settings extends ManagerController
+{
+    /**
+     * Settings constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
