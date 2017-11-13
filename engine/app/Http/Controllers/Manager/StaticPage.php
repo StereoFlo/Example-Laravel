095d3d560c1df4e8af70926b7692f51e36550d77
StereoFlo <StereoFlo@users.noreply.github.com>
2017-11-13 22:11:03 +0300

    static page manager controller

--- engine/app/Http/Controllers/Manager/StaticPage.php
+++ engine/app/Http/Controllers/Manager/StaticPage.php
@@ -3,6 +3,7 @@
 namespace RecycleArt\Http\Controllers\Manager;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 use RecycleArt\Http\Controllers\Controller;
 
 class StaticPage extends Controller
@@ -18,12 +19,20 @@ class StaticPage extends Controller
 
     public function makeNew()
     {
-
+        return view('manager.staticPage.form');
     }
 
+    /**
+     * todo необходимо записывать в сессию результат
+     *
+     * @param Request $request
+     *
+     * @return mixed
+     */
     public function process(Request $request)
     {
-
+        \RecycleArt\Models\StaticPage::getInstance()->updateOrMake($request);
+        return Redirect::to(route('managerPageList'));
     }
 
     public function remove(string $slug)
@@ -31,8 +40,17 @@ class StaticPage extends Controller
         return \RecycleArt\Models\StaticPage::getInstance()->removePage($slug);
     }
 
+    /**
+     * @param string $slug
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function update(string $slug)
     {
-
+        $page = \RecycleArt\Models\StaticPage::getInstance()->getBy($slug);
+        if (empty($page)) {
+            abort(404, 'page not found');
+        }
+        return view('manager.staticPage.form', ['page' => $page]);
     }
 }
