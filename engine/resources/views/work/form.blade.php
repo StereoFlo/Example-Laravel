b2bc267da7bcb089c2465cd9e072e28ca8779a89
StereoFlo <StereoFlo@users.noreply.github.com>
2017-11-13 20:32:46 +0300

    the work form style fix

--- engine/resources/views/work/form.blade.php
+++ engine/resources/views/work/form.blade.php
@@ -2,135 +2,137 @@
 
 @section('content')
 
-<section class="workAdd">
-    <div class="container">
+    <section class="workAdd">
+        <div class="container">
 
-        <div class="sectionTitle">
-            <h2>@lang('work.new')</h2>
-        </div>
+            <div class="sectionTitle">
+                <h2>@lang('work.new')</h2>
+            </div>
 
-        <form method="post" action="{{ route('workProcess') }}" enctype="multipart/form-data" class="form workAddForm">
-            {{ csrf_field() }}
+            <form method="post" action="{{ route('workProcess') }}" enctype="multipart/form-data" class="form workAddForm">
+                {{ csrf_field() }}
 
-            <div class="inputGroup{{ $errors->has('workName') ? ' has-error' : '' }}">
-                <label for="workName">@lang('work.nameOfNewWork'):</label>
-                <input id="workName" type="text" name="workName" value="{{ !empty($work['workName']) ? $work['workName'] : null }}" required>
-                <span class="errorText">
+                <div class="inputGroup{{ $errors->has('workName') ? ' has-error' : '' }}">
+                    <label for="workName">@lang('work.nameOfNewWork'):</label>
+                    <input id="workName" type="text" name="workName" value="{{ !empty($work['workName']) ? $work['workName'] : null }}" required>
+                    <span class="errorText">
                 @if ($errors->has('workName'))
-                        <strong>{{ $errors->first('workName') }}</strong>
-                    @endif
+                            <strong>{{ $errors->first('workName') }}</strong>
+                        @endif
             </span>
-            </div>
+                </div>
 
-            <div class="inputGroup{{ $errors->has('description') ? ' has-error' : '' }}">
-                <label for="desc">@lang('work.descriptionOfNewWork'):</label>
-                <textarea id="desc" name="description" cols="80" rows="8" required>{{ !empty($work['description']) ? $work['description'] : null }}</textarea>
-                <span class="errorText">
+                <div class="inputGroup{{ $errors->has('description') ? ' has-error' : '' }}">
+                    <label for="desc">@lang('work.descriptionOfNewWork'):</label>
+                    <textarea id="desc" name="description" cols="80" rows="8"
+                              required>{{ !empty($work['description']) ? $work['description'] : null }}</textarea>
+                    <span class="errorText">
                 @if ($errors->has('description'))
-                        <strong>{{ $errors->first('description') }}</strong>
-                    @endif
+                            <strong>{{ $errors->first('description') }}</strong>
+                        @endif
             </span>
-            </div>
+                </div>
 
-            <div class="inputGroup{{ $errors->has('tags') ? ' has-error' : '' }}">
-                <label for="tags">Теги:</label>
-                <input id="tags" type="text" name="tags" placeholder="добавьте теги через ',' ">
-                <span class="errorText">
+                <div class="inputGroup{{ $errors->has('tags') ? ' has-error' : '' }}">
+                    <label for="tags">Теги:</label>
+                    <input id="tags" type="text" name="tags" placeholder="добавьте теги через ',' ">
+                    <span class="errorText">
                 @if ($errors->has('tags'))
-                        <strong>{{ $errors->first('tags') }}</strong>
-                @endif
+                            <strong>{{ $errors->first('tags') }}</strong>
+                        @endif
             </span>
-                @if(!empty($work['tags']))
-                    <p>Теги работы:</p>
-                    <div class="tag">
-                        @foreach($work['tags'] as $tag)
-                            <a id="tag_{{ $tag['id'] }}" href="{{ route('deleteFromWork', ['tagId' => $tag['id'], 'workId' => $work['id']]) }}" class="tag__item">
-                                <span class="name">{{ $tag['tag'] }}</span>
-                                <i class="fa fa-trash" aria-hidden="true"></i>
-                            </a>
-                        @endforeach
-                    </div>
-                @endif
-            </div>
+                    @if(!empty($work['tags']))
+                        <p>Теги работы:</p>
+                        <div class="tag">
+                            @foreach($work['tags'] as $tag)
+                                <a id="tag_{{ $tag['id'] }}" href="{{ route('deleteFromWork', ['tagId' => $tag['id'], 'workId' => $work['id']]) }}"
+                                   class="tag__item">
+                                    <span class="name">{{ $tag['tag'] }}</span>
+                                    <i class="fa fa-trash" aria-hidden="true"></i>
+                                </a>
+                            @endforeach
+                        </div>
+                    @endif
+                </div>
 
-            @if(!empty($work['id']))
-                <p>Категории работы:</p>
-                @if(empty($work['categories']['inWork']))
-                    <p>У этой работы не категорий</p>
-                @else
-                    <div class="category">
-                        @foreach($work['categories']['inWork'] as $category)
-                            <a href="" class="category__item">
-                                <span class="name">{{ $category['name'] }}</span>
-                                <i class="fa fa-trash" aria-hidden="true"></i>
-                            </a>
-                        @endforeach
-                    </div>
-                @endif
-                <p>Все категории:</p>
-                @if(empty($work['categories']['notInWork']))
-                    <p>У нас пока нет категорий</p>
+                @if(!empty($work['id']))
+                    <p>Категории работы:</p>
+                    @if(empty($work['categories']['inWork']))
+                        <p>У этой работы не категорий</p>
+                    @else
+                        <div class="category">
+                            @foreach($work['categories']['inWork'] as $category)
+                                <a href="" class="category__item">
+                                    <span class="name">{{ $category['name'] }}</span>
+                                    <i class="fa fa-trash" aria-hidden="true"></i>
+                                </a>
+                            @endforeach
+                        </div>
+                    @endif
+                    <p>Все категории:</p>
+                    @if(empty($work['categories']['notInWork']))
+                        <p>У нас пока нет категорий</p>
+                    @else
+                        <div class="inputGroup checkboxes">
+                            @foreach($work['categories']['notInWork'] as $category)
+                                <input id="{{ $category['id'] }}" type="checkbox" name="categories[]" value="{{ $category['id'] }}">
+                                <label for="{{ $category['id'] }}">{{ $category['name'] }}</label>
+                            @endforeach
+                        </div>
+                    @endif
+                    <input type="hidden" name="workId" value="{{ $work['id'] }}">
                 @else
                     <div class="inputGroup checkboxes">
-                        @foreach($work['categories']['notInWork'] as $category)
+                        @foreach($categories as $category)
                             <input id="{{ $category['id'] }}" type="checkbox" name="categories[]" value="{{ $category['id'] }}">
                             <label for="{{ $category['id'] }}">{{ $category['name'] }}</label>
                         @endforeach
                     </div>
                 @endif
-                <input type="hidden" name="workId" value="{{ $work['id'] }}">
-            @else
-                <div class="inputGroup checkboxes">
-                    @foreach($categories as $category)
-                        <input id="{{ $category['id'] }}" type="checkbox" name="categories[]" value="{{ $category['id'] }}">
-                        <label for="{{ $category['id'] }}">{{ $category['name'] }}</label>
-                    @endforeach
-                </div>
-            @endif
 
-            <div class="inputGroup{{ $errors->has('images') ? ' has-error' : '' }}">
-                <label for="images">@lang('work.photoOfNewWork') (16:9, jpeg):</label>
-                <div class="filearea">
-                    <span>@lang('work.photoDescrOfNewWork')</span>
-                    <input type="file" name="images[]" value="{{ old('images') }}" multiple {{ !empty($errors->has('images')) ? 'required' : null }}>
-                </div>
-                <div class="fileareaPreview"></div>
-                <span class="errorText">
-                @if ($errors->has('images'))
-                        <strong>{{ $errors->first('images') }}</strong>
-                @endif
-            </span>
-                @if(!empty($work['images']))
-                    <div class="imageGroup">
-                        @foreach($work['images'] as $image)
-
-                            @if($image['isDefault'])
-                                <div class="image">
-                                    <div class="default">
-                                        <i class="fa fa-check-square" aria-hidden="true"></i>
-                                    </div>
-                                    <img src="{{ $image['link'] }}" alt="">
-                                    <a href="{{ route('imageDeleteFromWork', ['workId' => $work['id'], 'imageId' => $image['id']]) }}" class="del">
-                                        <i class="fa fa-trash" aria-hidden="true"></i>
-                                    </a>
-                                </div>
-                            @else
-                                <div class="image">
-                                    <img src="{{ $image['link'] }}" alt="">
-                                    <a href="{{ route('imageDeleteFromWork', ['workId' => $work['id'], 'imageId' => $image['id']]) }}" class="del">
-                                        <i class="fa fa-trash" aria-hidden="true"></i>
-                                    </a>
-                                </div>
-                            @endif
-                        @endforeach
+                <div class="inputGroup{{ $errors->has('images') ? ' has-error' : '' }}">
+                    <label for="images">@lang('work.photoOfNewWork') (16:9, jpeg):</label>
+                    <div class="filearea">
+                        <span>@lang('work.photoDescrOfNewWork')</span>
+                        <input type="file" name="images[]" value="{{ old('images') }}" multiple {{ !empty($errors->has('images')) ? 'required' : null }}>
                     </div>
-                @endif
-            </div>
+                    <div class="fileareaPreview"></div>
+                    <span class="errorText">
+                @if ($errors->has('images'))
+                            <strong>{{ $errors->first('images') }}</strong>
+                        @endif
+            </span>
+                    @if(!empty($work['images']))
+                        <div class="imageGroup">
+                            @foreach($work['images'] as $image)
 
-            <button type="submit" name="button" class="button">@lang('work.buttonOfNewWork')</button>
-        </form>
-    </div>
-</section>
+                                @if($image['isDefault'])
+                                    <div class="image">
+                                        <div class="default">
+                                            <i class="fa fa-check-square" aria-hidden="true"></i>
+                                        </div>
+                                        <img src="{{ $image['link'] }}" alt="">
+                                        <a href="{{ route('imageDeleteFromWork', ['workId' => $work['id'], 'imageId' => $image['id']]) }}" class="del">
+                                            <i class="fa fa-trash" aria-hidden="true"></i>
+                                        </a>
+                                    </div>
+                                @else
+                                    <div class="image">
+                                        <img src="{{ $image['link'] }}" alt="">
+                                        <a href="{{ route('imageDeleteFromWork', ['workId' => $work['id'], 'imageId' => $image['id']]) }}" class="del">
+                                            <i class="fa fa-trash" aria-hidden="true"></i>
+                                        </a>
+                                    </div>
+                                @endif
+                            @endforeach
+                        </div>
+                    @endif
+                </div>
+
+                <button type="submit" name="button" class="button">@lang('work.buttonOfNewWork')</button>
+            </form>
+        </div>
+    </section>
 @endsection
 
 
