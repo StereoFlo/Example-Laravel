fc3665aab79abda63667cd17d3b98e08eb017ede
Dmitriy <silaevd@yandex.ru>
2017-11-16 21:21:10 +0300

    static pages fix

--- engine/resources/views/static_page/main.blade.php
+++ engine/resources/views/static_page/main.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.app')
 @section('content')
+    <section class="staticPageContainer">
+        <div class="container">
 
-    {!! $page !!}
+            <div class="sectionTitle">
+                <h2>{{ $page['name'] }}</h2>
+            </div>
+            {!! $page['content'] !!}
+        </div>
+    </section>
 @endsection
