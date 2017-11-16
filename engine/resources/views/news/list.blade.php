c4ec0ce29e6ef342627d3277553ce072461ce7db
Evgeniy <e.bronnikov@petrovich.ru>
2017-11-16 15:41:17 +0300

    pagination fix

--- engine/resources/views/news/list.blade.php
+++ engine/resources/views/news/list.blade.php
@@ -23,26 +23,19 @@
                 </div>
             @endif
 
-            <div class="allNews__controls">
+            <div class="pagination">
                 @if($currentPage > 0)
-                    <a href="{{ url('news/page', ['id' => $currentPage - 1]) }}" class="allNews__prew">
+                    <a href="{{ url('news/page', ['id' => $currentPage - 1]) }}" class="previous">
                         <i class="fa fa-arrow-left" aria-hidden="true"></i>
                     </a>
                 @endif
-                @if(($newsCount / $parPage) > 1 && ($newsCount / $parPage) > $currentPage)
-                    <a href="{{ url('news/page', ['id' => $currentPage + 1]) }}" class="allNews__next">
+                    <a class="active">{{ $currentPage+1 }}</a>
+                @if(($newsCount / $parPage) > 1 && ($newsCount / $parPage) > $currentPage+1)
+                    <a href="{{ url('news/page', ['id' => $currentPage + 1]) }}" class="next">
                         <i class="fa fa-arrow-right" aria-hidden="true"></i>
                     </a>
                 @endif
             </div>
         </div>
     </section>
-
-    <div class="pagination">
-        <a href="#" class="previous">&laquo;</a>
-        <a href="#">1</a>
-        <a href="#" class="active">2</a>
-        <a href="#">3</a>
-        <a href="#" class="next">&raquo;</a>
-    </div>
 @endsection
