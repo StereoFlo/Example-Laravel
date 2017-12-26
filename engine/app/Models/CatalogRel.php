e03f16d88feb434d3241b3c238296b13dfc499b7
Evgeniy <e.bronnikov@mayer-team.ru>
2017-12-26 10:40:35 +0300

    relation for catalog for future

--- engine/app/Models/CatalogRel.php
+++ engine/app/Models/CatalogRel.php
@@ -37,6 +37,23 @@ class CatalogRel extends Model
     }
 
     /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function relation()
+    {
+        return $this->hasMany(Catalog::class, 'id', 'catalog_id');
+    }
+
+    /**
+     * @param int $workId
+     * @return \Illuminate\Database\Eloquent\Collection|static[]
+     */
+    public function getByWorkId(int $workId)
+    {
+        return $this->with('relation')->where('catalog_rel.work_id', $workId)->get();
+    }
+
+    /**
      * @param int|array $category
      * @param int   $workId
      *
