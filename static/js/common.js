8772d7f85992293c20a588635d3a9b3e5c1d0371
Dmitry.Silaev <silaevd@yandex.ru>
2018-01-29 13:30:00 +0300

    fileuploader translation; change slider styles

--- static/js/common.js
+++ static/js/common.js
@@ -200,7 +200,63 @@ $(function () {
                 if(item.format == 'image') {
                     item.html.find('.fileuploader-item-icon').hide();
                 }
-            }
+            },
+            popup: {
+                // popup HTML {String, Function}
+                template: function (data) {
+                    return '<div class="fileuploader-popup">' +
+                        '<div class="fileuploader-popup-preview">' +
+                        '<div class="node ${format}">${reader.node}</div>' +
+                        '<div class="tools">' +
+                        '<ul>' +
+                        '<li>' +
+                        '<span>${captions.name}:</span>' +
+                        '<h5>${name}</h5>' +
+                        '</li>' +
+                        '<li>' +
+                        '<span>${captions.size}:</span>' +
+                        '<h5>${size2}</h5>' +
+                        '</li>' +
+                        (data.reader && data.reader.width ? '<li>' +
+                                '<span>${captions.dimensions}:</span>' +
+                                '<h5>${reader.width}x${reader.height}px</h5>' +
+                                '</li>' : ''
+                        ) +
+                        (data.reader && data.reader.duration ? '<li>' +
+                                '<span>${captions.duration}:</span>' +
+                                '<h5>${reader.duration2}</h5>' +
+                                '</li>' : ''
+                        ) +
+                        '<li class="separator"></li>' +
+                        (data.format == 'image' && data.reader.src && data.editor ? '<li>' +
+                                '<a data-action="crop">' +
+                                '<i></i>' +
+                                '<span>${captions.crop}</span>' +
+                                '</a>' +
+                                '</li>' +
+                                '<li>' +
+                                '<a data-action="rotate-cw">' +
+                                '<i></i>' +
+                                '<span>${captions.rotate}</span>' +
+                                '</a>' +
+                                '</li>' : ''
+                        ) +
+                        '<li>' +
+                        '<a data-action="remove">' +
+                        '<i></i>' +
+                        '<span>${captions.remove}</span>' +
+                        '</a>' +
+                        '</li>' +
+                        '</ul>' +
+                        '<div class="buttons">' +
+                        '<a class="fileuploader-popup-button" data-action="cancel">${captions.cancel}</a>' +
+                        '<a class="fileuploader-popup-button button-success" data-action="save">${captions.confirm}</a>' +
+                        '</div>' +
+                        '</div>' +
+                        '</div>' +
+                        '</div>';
+                },
+            },
         },
         afterRender: function(listEl, parentEl, newInputEl, inputEl) {
             const plusInput = listEl.find('.fileuploader-thumbnails-input'),
@@ -278,6 +334,27 @@ $(function () {
             //     });
             // }
         },
+        captions: {
+            confirm: 'Ок',
+            cancel: 'Отмена',
+            name: 'Имя',
+            type: 'Тип',
+            size: 'Размер',
+            dimensions: 'Пропорции',
+            crop: 'Обрезать',
+            rotate: 'Повернуть',
+            download: 'Скачать',
+            remove: 'Удалить',
+            removeConfirmation: 'Вы действительно хотите удалить файл ?',
+            errors: {
+                filesLimit: 'Только ${limit} файлов может быть загружено.',
+                filesType: 'Только ${extensions} файлы могут быть загружены.',
+                fileSize: '${name} слишком большой! Выбирите фалй не более ${fileMaxSize}MB.',
+                filesSizeAll: 'Файлы, которые вы выбрали, слишком велики! Пожалуйста, загрузите файлы до ${maxSize} MB.',
+                fileName: 'Файл с именем ${name} уже выбран.',
+                folderUpload: 'Вы не можете загружать папки.'
+            }
+        }
     });
 
     /* ---------------------------------------------- /*
