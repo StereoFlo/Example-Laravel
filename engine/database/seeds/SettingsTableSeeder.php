bdc1e05434f275afb193600b34d15bbefdb8edef
StereoFlo <StereoFlo@users.noreply.github.com>
2018-02-06 22:45:49 +0300

    fixes

--- engine/database/seeds/SettingsTableSeeder.php
+++ engine/database/seeds/SettingsTableSeeder.php
@@ -7,18 +7,18 @@ class SettingsTableSeeder extends Seeder
 {
     public function run()
     {
-        $setting = new \RecycleArt\Models\Settings();
-        $setting->setting_slug = 'slogan';
-        $setting->setting_name = 'Слоган';
-        $setting->setting_value = 'some content here...';
-        $setting->save();
-        $setting = null;
-
-        $setting = new \RecycleArt\Models\Settings();
-        $setting->setting_slug = 'limitWorksForGallery';
-        $setting->setting_name = 'Количество работ на странице галлереи';
-        $setting->setting_value = '30';
-        $setting->save();
+//        $setting = new \RecycleArt\Models\Settings();
+//        $setting->setting_slug = 'slogan';
+//        $setting->setting_name = 'Слоган';
+//        $setting->setting_value = 'some content here...';
+//        $setting->save();
+//        $setting = null;
+//
+//        $setting = new \RecycleArt\Models\Settings();
+//        $setting->setting_slug = 'limitWorksForGallery';
+//        $setting->setting_name = 'Количество работ на странице галлереи';
+//        $setting->setting_value = '30';
+//        $setting->save();
 
         $setting = new \RecycleArt\Models\Settings();
         $setting->setting_slug = 'generalPageBlock';
