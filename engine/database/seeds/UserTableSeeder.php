51338755bd62c2b8158a6eda37c5e0ee41cd0e20
StereoFlo <StereoFlo@users.noreply.github.com>
2018-02-14 22:21:34 +0300

    fixes

--- engine/database/seeds/UserTableSeeder.php
+++ engine/database/seeds/UserTableSeeder.php
@@ -13,29 +13,29 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        $role_admin = Role::where('name', User::ROLE_ADMIN)->first();
-        $role_moderator  = Role::where('name', User::ROLE_MODERATOR)->first();
-        $role_author  = Role::where('name', User::ROLE_AUTHOR)->first();
-
-        $admin = new User();
-        $admin->name = 'admin';
-        $admin->email = 'admin@recycle.lan';
-        $admin->password = \bcrypt('admin');
-        $admin->save();
-        $admin->roles()->attach($role_admin);
-
-        $manager = new User();
-        $manager->name = 'moderator';
-        $manager->email = 'moderator@recycle.lan';
-        $manager->password = \bcrypt('moderator');
-        $manager->save();
-        $manager->roles()->attach($role_moderator);
-
-        $author = new User();
-        $author->name = 'author';
-        $author->email = 'author@recycle.lan';
-        $author->password = \bcrypt('author');
-        $author->save();
-        $author->roles()->attach($role_author);
+//        $role_admin = Role::where('name', User::ROLE_ADMIN)->first();
+//        $role_moderator  = Role::where('name', User::ROLE_MODERATOR)->first();
+//        $role_author  = Role::where('name', User::ROLE_AUTHOR)->first();
+//
+//        $admin = new User();
+//        $admin->name = 'admin';
+//        $admin->email = 'admin@recycle.lan';
+//        $admin->password = \bcrypt('admin');
+//        $admin->save();
+//        $admin->roles()->attach($role_admin);
+//
+//        $manager = new User();
+//        $manager->name = 'moderator';
+//        $manager->email = 'moderator@recycle.lan';
+//        $manager->password = \bcrypt('moderator');
+//        $manager->save();
+//        $manager->roles()->attach($role_moderator);
+//
+//        $author = new User();
+//        $author->name = 'author';
+//        $author->email = 'author@recycle.lan';
+//        $author->password = \bcrypt('author');
+//        $author->save();
+//        $author->roles()->attach($role_author);
     }
 }
