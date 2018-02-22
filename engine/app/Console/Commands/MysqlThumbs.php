23df3bf4fc6ceb89d855e74e649991e3aa68f590
Evgeniy <e.bronnikov@mayer-team.ru>
2018-02-22 15:50:17 +0300

    command for mysql thumbs

---
+++ engine/app/Console/Commands/MysqlThumbs.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace RecycleArt\Console\Commands;
+
+use Illuminate\Console\Command;
+use RecycleArt\Models\WorkImages;
+
+class MysqlThumbs extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:thumbs_db';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $images = (new WorkImages())->getList();
+        if (empty($images)) {
+            return 'nothing todo';
+        }
+        foreach ($images as $image) {
+            $linkData = explode('/', $image['link']);
+            if (!\is_numeric($linkData[1]) || !\is_numeric($linkData[3])) {
+                continue;
+            }
+            $thumbPath = \str_replace('engine/public/', '', \public_path(\sprintf(WorkImages::THUMB_PATH, $linkData[1], $linkData[3])));
+            if (empty($image['link']) && \file_exists($thumbPath . '/' . $linkData[4])) {
+                $image = WorkImages::find($image['id']);
+                $image->thumb = $thumbPath . '/' . $linkData[4];
+                $image->save();
+            }
+        }
+        return 'OK!' . PHP_EOL;
+    }
+}
