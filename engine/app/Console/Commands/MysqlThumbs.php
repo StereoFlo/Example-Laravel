8e185ad872fe59306fc4efaca72a90fd2c83d66a
Evgeniy <e.bronnikov@mayer-team.ru>
2018-02-22 15:58:02 +0300

    command for mysql thumbs

--- engine/app/Console/Commands/MysqlThumbs.php
+++ engine/app/Console/Commands/MysqlThumbs.php
@@ -4,7 +4,6 @@ namespace RecycleArt\Console\Commands;
 
 use Illuminate\Console\Command;
 use RecycleArt\Models\WorkImages;
-use SebastianBergmann\CodeCoverage\Report\PHP;
 
 class MysqlThumbs extends Command
 {
@@ -50,15 +49,11 @@ class MysqlThumbs extends Command
             }
             $thumbPath = \str_replace('engine/public/', '', \public_path(\sprintf(WorkImages::THUMB_PATH, $linkData[1], $linkData[3])));
             print $thumbPath . '/' . $linkData[4] . PHP_EOL;
-            if (empty($image['link'])) {
-                print 'db is empty' . PHP_EOL;
-//                print 'OK' . PHP_EOL;
-//                $image = WorkImages::find($image['id']);
-//                $image->thumb = $thumbPath . '/' . $linkData[4];
-//                $image->save();
-            }
-            if (\file_exists($thumbPath . '/' . $linkData[4])) {
-                print 'file_exists' . PHP_EOL;
+            if (empty($image['thumb']) && \file_exists($thumbPath . '/' . $linkData[4])) {
+                print 'OK' . PHP_EOL;
+                $image = WorkImages::find($image['id']);
+                $image->thumb = $thumbPath . '/' . $linkData[4];
+                $image->save();
             }
         }
         return 'OK!' . PHP_EOL;
