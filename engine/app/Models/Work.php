5ffaac46f0db18db46696964303fb20db8fc21e5
Evgeniy <evgeniy@stereoflo.ru>
2018-07-02 15:37:04 +0300

    manager pagination

--- engine/app/Models/Work.php
+++ engine/app/Models/Work.php
@@ -252,7 +252,7 @@ class Work extends Model
             ->select('work.*', 'users.id as userId', 'users.name as userName')
             ->join('users', 'users.id', '=', 'work.userId')
             ->orderBy('work.id', 'desc')
-            ->offset($offset)
+            ->skip($offset*$limit)
             ->limit($limit)
             ->get();
         if (!$this->checkEmptyObject($result)) {
