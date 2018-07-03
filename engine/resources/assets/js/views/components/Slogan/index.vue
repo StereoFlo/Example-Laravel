2303f4e00ccd5571f2993b79ca8f2a1b62ea5bdc
StereoFlo <StereoFlo@users.noreply.github.com>
2018-07-03 23:30:04 +0300

    edittor

--- engine/resources/assets/js/views/components/Slogan/index.vue
+++ engine/resources/assets/js/views/components/Slogan/index.vue
@@ -1,7 +1,11 @@
 <template src="./index.html"></template>
 
 <script>
+    import { VueEditor } from 'vue2-editor'
     export default {
+        components: {
+            VueEditor
+        },
         created() {
         }
     }
